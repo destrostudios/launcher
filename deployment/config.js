63f4ec9d8700b795aaa298780c7d7d4c08bca5af
carlkuesters <carl.kuesters@youengineering.com>
2020-05-27 01:33:38 +0200

    Remove old deployment files.

--- deployment/config.js
+++
@@ -1,8 +0,0 @@
-module.exports.serverCredentials = {
-  host: 'host',
-  port: 1234,
-  username: 'username',
-  password: 'password'
-};
-module.exports.localDirectory = './dist/destrostudios-launcher/';
-module.exports.remoteDirectory = 'remoteDirectory/';
